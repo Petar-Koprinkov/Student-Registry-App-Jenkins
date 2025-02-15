b51a07b3493d378131109d0801a94b9dd58469e3
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-02-15 22:16:07 +0200

    feat: demo

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file Some change, test again
+// .mocharc.js - Mocha config file
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
